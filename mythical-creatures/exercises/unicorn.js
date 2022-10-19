471d44fe2bc68fc3f5940c18b49c3aba5657b563
Derek Yeh <derek.yeh91@gmail.com>
2022-10-19 13:27:16 -0700

    update changes2

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,18 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
+    // console.log(this)
+  }
+  // need to create this function outside of the construtor
+  isWhite() {
+    return false
+  };
+
+  says(expression) {
+    return `**;* ${expression} *;**`
   }
 }
+
 
 module.exports = Unicorn;
