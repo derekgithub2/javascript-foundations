9cbf7d81088030a2478e88cbec5154cc99fc6d90
Derek Yeh <derek.yeh91@gmail.com>
2022-10-20 13:43:10 -0700

    update dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,29 @@
+class Dragon {
+  constructor(dragonName, dragonRider) {
+    this.name = dragonName;
+    this.rider = dragonRider;
+    this.hungry = true;
+    console.log(this)
+  }
 
+  greet () {
+    return `Hi, ${this.rider}!`
+  }
+
+  // eat function: first start internal counter 
+  // if this.hungry is less than 3, then return true, else return false
+
+  eat () {
+    var count = 0;
+    if (count < 3) {
+      this.hungry = true;
+      count++;
+    } else {
+      this.hungry = false;
+    }
+    console.log(count)
+    return count;
+  }
+}
+
+module.exports = Dragon;
