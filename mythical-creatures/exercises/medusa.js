0a1a3e941ef16042f770748a5dafd56e65f32bbc
Derek Yeh <derek.yeh91@gmail.com>
2022-10-25 12:02:23 -0700

    finish past medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor (medusaName) {
+        this.name = medusaName
+        this.statues = []
+    }
+
+    gazeAtVictim(victim){
+        if (this.statues.length < 3) {
+            this.statues.push(new Statue(victim.name))
+        } else {
+            var savedVictim = this.statues.splice(0,1)
+            this.statues.push(new Statue(victim.name))
+            var freedPerson = new Person(savedVictim[0].name)
+            freedPerson.mood = 'relieved'
+            return freedPerson
+            // return new Person(savedVictim[0].name, savedVictim[0].mood = 'relieved')
+        }
+        // console.log(this.statues)
+    }
+}
+
+module.exports = Medusa;
